8d263d51b995861e4832161e065b880dd6bf27f2
Filippo Ziche <filippo.ziche@gmail.com>
2023-02-02 20:08:42 +0100

    migrating from autowire drivers enumeration to custom one

--- src/main/java/it/univr/satella/drivers/SensorDriverPublish.java
+++ src/main/java/it/univr/satella/drivers/SensorDriverPublish.java
@@ -3,10 +3,16 @@ package it.univr.satella.drivers;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * All sensor drivers implementations with this annotation are
  * automatically loaded by the system at startup and added to the
  * sensor driver repository.
  */
-@Qualifier
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface SensorDriverPublish { }
--- src/main/java/it/univr/satella/drivers/SensorDriverRepository.java
+++ src/main/java/it/univr/satella/drivers/SensorDriverRepository.java
@@ -3,11 +3,20 @@ package it.univr.satella.drivers;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.cglib.beans.BeanGenerator;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ClassUtils;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * Repository responsible for loading and storing all published sensor drivers
@@ -19,14 +28,44 @@ public class SensorDriverRepository {
      * All published sensor drivers
      */
     @Autowired
-    @SensorDriverPublish
+    //@SensorDriverPublish
     private List<SensorDriver> publishedSensorDrivers;
+
+    /**
+     * All published sensors ready to be instantiated
+     */
+    private List<BeanDefinition> publishedSensorDriverBeans;
 
     @PostConstruct
     public void printSensorDrivers() {
         Logger log = LoggerFactory.getLogger(SensorDriverRepository.class);
         for (ISensorDriver driver : publishedSensorDrivers)
             log.info("Found driver: " + driver.getId());
+
+        // Create a scanner for all classes with the SensorDriverPublish annotation
+        ClassPathScanningCandidateComponentProvider scanner;
+        scanner = new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AnnotationTypeFilter(SensorDriverPublish.class));
+        Set<BeanDefinition> beanDefs = scanner
+                .findCandidateComponents("it.univr.satella.drivers.implementation");
+
+        // Save all beans ready for construction
+        for (BeanDefinition bean : beanDefs) {
+            try {
+
+                String driverName = bean.getBeanClassName();
+                Class<?> c = BeanGenerator.class.getClassLoader().loadClass(driverName);
+                ISensorDriver driver = (ISensorDriver)c.getConstructor().newInstance();
+                System.out.print(driver.getId());
+
+
+            } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+                     InvocationTargetException e) {
+
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
     /**
@@ -39,7 +78,3 @@ public class SensorDriverRepository {
                 .findFirst();
     }
 }
-
-
-
-
