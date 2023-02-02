f52d905b9186970376421d2d3e7e3811d2a459bd
Filippo Ziche <filippo.ziche@gmail.com>
2023-02-02 13:48:44 +0100

    init springboot project

---
+++ src/main/java/it/univr/satella/SatellaApplication.java
@@ -0,0 +1,13 @@
+package it.univr.satella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/univr/satella/SatellaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univr.satella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
